55161c7d93881e32051fd0324fae17e00106a617
Gitea <gitea@fake.local>
2023-06-10 19:35:53 +0800

    update log and main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,26 +8,34 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
+        <div className='hero__logo'>
+          <img className={styles.heroLogoImg} src='/img/runtime-land-logo-240.svg' alt='runtime-land-logo' />
+        </div>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          <p>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              Start By One Line
+            </Link>
+          </p>
         </div>
+        <p>
+          <strong>this site is under construction</strong>
+        </p>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
